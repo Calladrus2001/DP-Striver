b72202da26d78d9df8ce2b903c722b210eeb29be
Calladrus2001 <dugarvishesh@gmail.com>
2022-12-17 14:54:39 +0530

    14_countSubset memoiz 11/13

---
+++ 14_countSubset/memoiz.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+int findWays(vector<int> &num, int tar)
+{
+  vector<vector<int>> dp(num.size(), vector<int>(tar + 1, -1));
+  int helper(vector<int> & num, int target, int idx, vector<vector<int>> &dp);
+  return helper(num, tar, num.size() - 1, dp);
+}
+
+int helper(vector<int> &num, int target, int idx, vector<vector<int>> &dp)
+{
+  if (target < 0)
+    return 0;
+  if (target == 0)
+    return 1;
+  if (idx < 0)
+    return 0;
+
+  if (dp[idx][target] != -1)
+    return dp[idx][target];
+
+  int notPick = helper(num, target, idx - 1, dp);
+  int pick = helper(num, target - num[idx], idx - 1, dp);
+
+  return dp[idx][target] = pick + notPick;
+}
